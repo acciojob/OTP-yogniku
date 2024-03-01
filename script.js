420b399422a826a1ce7679bc0118b6ef96eaa2cc
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-01 22:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (e) => {
+            const value = e.target.value;
+            if (value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && index > 0 && inputs[index].value === '') {
+                inputs[index - 1].focus();
+            }
+        });
+    });
